621d73deb2ca8cf505d75cc66a0ca1a8731b6ec3
alejandro <alejandrette5117@gmail.com>
2026-02-10 11:13:21 +0100

    Finish class usuario and start class prestamo(cosntructor and atributes)

--- src/prestamos/Prestamo.java
+++ src/prestamos/Prestamo.java
@@ -1,4 +1,31 @@
 package prestamos;
 
+import prestamos.usuarioException.PrestamoInvalidoException;
+
+import java.time.*;
+
 public class Prestamo {
+    private String codigoLibro;
+    private String tituloLibro;
+    private Usuario socio;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucionPrevista;
+    private LocalDate fechaDevolucionReal;
+
+    public Prestamo(String codigoLibro, String tituloLibro, Usuario socio, LocalDate fechaPrestamo) throws PrestamoInvalidoException {
+        if (!codigoLibro.matches("^LIB\\d{4}$")) throw new PrestamoInvalidoException("El formato del código de libro (LIB1234)");
+        if (tituloLibro.isEmpty()) throw new PrestamoInvalidoException("El título del libro no es válido");
+        if (fechaPrestamo == null || fechaPrestamo.isAfter(LocalDate.now())) throw new PrestamoInvalidoException("La fecha del préstamo es inválido");
+
+        this.codigoLibro = codigoLibro;
+        this.tituloLibro = tituloLibro;
+        this.socio = socio;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucionPrevista = fechaPrestamo.plusDays(14);
+        this.fechaDevolucionReal = null;
+    }
+
+    public void registrarDevolucion(LocalDate fecha){
+        if (fecha == null || fecha.isBefore(this.fechaPrestamo)) return;
+    }
 }
--- src/prestamos/Usuario.java
+++ src/prestamos/Usuario.java
@@ -2,9 +2,9 @@ package prestamos;
 
 import prestamos.usuarioException.*;
 
-import java.time.LocalDate;
+import java.time.*;
 
-public class Usuario {
+public class Usuario extends Utils {
     private String nombre;
     private String email;
     private String numeroSocio;
@@ -15,13 +15,15 @@ public class Usuario {
     public Usuario(String nombre, String email, String numeroSocio, LocalDate fechaRegistro) throws UsuarioInvalidoException {
         if (nombre.isEmpty()) throw new UsuarioInvalidoException("Falta nombre de usuario");
         if (!email.matches(".+@.\\..+")) throw new UsuarioInvalidoException("El formato de email es inválido (ejemplo@gmail.com)");
-        if (!numeroSocio.matches("SOC\\d{5}")) throw new UsuarioInvalidoException("El formato de número de sócio es inválido (SOC12345)");
-        //if (fechaRegistro.) throw new UsuarioInvalidoException("");
+        if (!numeroSocio.matches("^SOC\\d{5}$")) throw new UsuarioInvalidoException("El formato de número de sócio es inválido (SOC12345)");
+        if (fechaRegistro == null) throw new UsuarioInvalidoException("La fecha de registro está incompleta");
 
         this.nombre = nombre;
         this.email = email;
         this.numeroSocio = numeroSocio;
         this.fechaRegistro = fechaRegistro;
+        this.sancionado = false;
+        this.fechaFinSancion = null;
     }
 
     public void sancionar(int dias){
@@ -31,6 +33,7 @@ public class Usuario {
 
     public void levantarSancion(){
         this.sancionado = false;
+        this.fechaFinSancion = null;
     }
 
     public boolean estaSancionado(){
@@ -39,6 +42,7 @@ public class Usuario {
 
     @Override
     public String toString(){
-        return "Usuario: " + this.nombre + ", email: " + this.email + ", numero de sócio: " + this.numeroSocio + ", fecha de registro: ";
+        String msg = this.sancionado ? "Sancionado hasta = " + formatoFecha(this.fechaFinSancion) : "Sin sanción";
+        return "Usuario: " + this.nombre + ", email: " + this.email + ", numero de sócio: " + this.numeroSocio + ", fecha de registro: " + formatoFecha(this.fechaRegistro) + msg;
     }
 }
